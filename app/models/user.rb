f8066a97397d2a57c1b4857c77392751e4e19cdd
mohammad.anoos <mohammad.anoos@sumerusolutions.com>
2012-01-03 19:13:19 +0530

    changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :confirm_password, :confirm_msg
+  attr_accessible :username, :login
+  attr_accessor :login
   validates :login, :email, :password, :confirm_password, :presence => true
   validates :email, :uniqueness => true, :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+
+  def self.find_for_database_authentication(warden_conditions)
+   conditions = warden_conditions.dup
+   login = conditions.delete(:login)
+   where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.strip.downcase }]).first
+ end
+
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111220055648) do
+ActiveRecord::Schema.define(:version => 20120103125213) do
 
   create_table "users", :force => true do |t|
     t.string   "email",                                 :default => "", :null => false
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(:version => 20111220055648) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
